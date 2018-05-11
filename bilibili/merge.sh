4de9e4b754157bf7b53b4108a91187597132e4ee
RemiliaForever <remilia@koumakan.cc>
2018-05-11 16:15:55 +0800

    add file checking and autoscan

--- bilibili/merge.sh
+++ bilibili/merge.sh
@@ -1,25 +1,45 @@
 #!/bin/env bash
 
-# Make sure what you are doing, this script run without any checking.
+# Make sure what you are doing, this script run with little checking.
 
+PREFIX='xxxxxxxxxxxx'
+OUTPUT_PREFIX='xxxxxxxxxxxxx'
+SEARCH_RANGE=128
 
-PREFIX='file name prefix'
-
+_merge() {
 # create file list
-for f in $(ls ${PREFIX}\(${1}\)* -v)
+list=$(ls ${PREFIX}\(${1}\)* -v 2>/dev/null)
+if [[ -z "${list}" ]]; then
+    >&2 echo "${1} : NotFound."
+    return -1
+fi
+
+for f in ${list}
 do
+    if [[ ${f} =~ part$ ]]; then
+        [[ -e files.txt ]] && rm files.txt
+        echo "${1} : file is not download completed!"
+        return -1
+    fi
     echo "file '${f}'" >> files.txt
 done
 
 cat files.txt
 
 # merge
-ffmpeg -f concat -safe 0 -i files.txt -c copy "${PREFIX}-${1}.flv"
+ffmpeg -f concat -safe 0 -i files.txt -c copy "${OUTPUT_PREFIX}-${1}.flv"
 
 
 # clean
 rm ${PREFIX}\(${1}\)*
 rm files.txt
 
+}
 
-
+if [[ -n "${1}" ]]; then
+    return _merge $1
+else
+    for i in $(seq 1 ${SEARCH_RANGE}); do
+        _merge $i 2>/dev/null
+    done
+fi
