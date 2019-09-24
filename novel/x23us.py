a6457ae0d1827296f15dcd666d68d256b3b43eca
RemiliaForever <remilia@koumakan.cc>
2019-09-24 22:19:22 +0800

    add dedup

--- novel/x23us.py
+++ novel/x23us.py
@@ -2,6 +2,7 @@
 
 import sys
 import requests
+import re
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://www.x23us.com'
@@ -13,25 +14,36 @@ print(CURRENT_URL)
 s = requests.Session()
 
 r = s.get(BASE_URL + CURRENT_URL)
-res = BeautifulSoup(r.content)
+res = BeautifulSoup(r.content, 'html5lib')
 name = res.find(id='amain').find('dl').find('dt').findAll('a')[-1].string
 out = open(f'{name}.txt', 'w')
 
+last = ''
 while True:
     r = s.get(BASE_URL + CURRENT_URL)
-    res = BeautifulSoup(r.content)
+    res = BeautifulSoup(r.content, 'html5lib')
 
     title = res.find('h1').string
     print(f'parse {title}')
+    NEW_URL = res.find(id='footlink').find_all('a')[2].attrs['href']
+    if CURRENT_URL == NEW_URL:
+        print('got last')
+        break
+    CURRENT_URL = NEW_URL
+
+    if title == last:
+        print('duplicate, skip')
+        continue
+    last = title
+
     out.write(f'{title}\n')
 
     content = res.find(id='contents').strings
     for c in content:
+        c = re.sub('顶点小说.*更新最快', '', c)
         out.write(f'{c}\n')
 
     out.write('\n\n\n')
-
-    CURRENT_URL = res.find(id='footlink').find_all('a')[2].attrs['href']
 
     if CURRENT_URL[-4:] != 'html':
         break
